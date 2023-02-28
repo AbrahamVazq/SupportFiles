6053766c4de4e6d4b518fc3d95135d2f07d38da9
Moises Abraham Vazquez Perez <mavazquezp@elektra.com.mx>
2023-02-27 20:59:24 -0600

    [UPDATE] Update recomendations to call functions

--- SwiftViperTemplate/ViperModule.xctemplate/___FILEBASENAME___Protocols.swift
+++ SwiftViperTemplate/ViperModule.xctemplate/___FILEBASENAME___Protocols.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+/**
+ Sugerencias en el llamado de Funciones de Vista a Presenter
+ :condiciones:
+ 
+ Si queremos ir a otro modulo VIPER
+ 
+    * goTo___________( )
+    * goTo___________( _ : )
+ 
+ Si queremos ir por datos o hacer un consumo
+    * viewDidLoad()
+    * getTo___________( )
+    * getTo___________( _ : )
+ */
+
 // MARK: VIEW -> PRESENTER
 protocol ___VARIABLE_ModuleName____ViewToPresenterProtocol: AnyObject {
 	var view: ___VARIABLE_ModuleName____PresenterToViewProtocol? { get set }
@@ -14,19 +29,78 @@ protocol ___VARIABLE_ModuleName____ViewToPresenterProtocol: AnyObject {
 	var router: ___VARIABLE_ModuleName____PresenterToRouterProtocol? { get set }
 }
 
+//MARK: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/**
+ Sugerencias en el llamado de Funciones de  Presenter a Interactor
+ :condiciones:
+ 
+ Tratemos que la funcion se llame igual que desde la que viene en la vista al Presenter,
+ pero en esta ocacion agregemosle el subfijo: 'ToInteractor', para saber que vamos
+ del presenter al iintectartor
+ 
+    * get___________ToInteractor( )
+    * getTo___________ToInteractor( )
+ 
+    * get___________ToInteractor( _ : )
+    * getTo___________ToInteractor( _ : )
+ 
+ */
+
 // MARK: PRESENTER -> INTERACTOR
 protocol ___VARIABLE_ModuleName____PresenterToInteractorProtocol: AnyObject {
     var presenter: ___VARIABLE_ModuleName____InteractorToPresenterProtocol? { get set }
 }
 
+//MARK: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+/**
+ Sugerencias en el llamado de Funciones de Vista a Presenter
+ :condiciones:
+ 
+ Tratemos que la funcion se llame igual que desde la que viene en la vista al Presenter,
+ pero en esta ocacion agregemosle el subfijo: 'FromInteractor' con los parametros que correspondan,
+ para saber que vamos del Interactor  al Presenter
+ 
+    * get___________FromInteractor( )
+    * getTo___________FromInteractor( )
+ 
+    * get___________FromInteractor(_ :)
+    * getTo___________FromInteractor(_ :)
+ */
+
 // MARK: INTERACTOR -> PRESENTER
 protocol ___VARIABLE_ModuleName____InteractorToPresenterProtocol: AnyObject {
 }
+
+//MARK: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/**
+ Sugerencias en el llamado de Funciones de Vista a Presenter
+ :condiciones:
+ 
+ Tratemos de usar la funcion updateView(_ :), con los parametros que correspondan, para
+ saber que vamos del Presenter a la Vista.
+ 
+    * updateView(_: )
+ 
+ */
 
 // MARK: PRESENTER -> VIEW
 protocol ___VARIABLE_ModuleName____PresenterToViewProtocol: AnyObject {
     var presenter: ___VARIABLE_ModuleName____ViewToPresenterProtocol? { get set }
 }
+
+//MARK: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/**
+ Sugerencias en el llamado de Funciones de Vista a Presenter
+ :condiciones:
+ 
+ Si queremos ir a otro modulo VIPER, usemos goTo_________( _ : ), con parametros si es necesario para
+ asegurar que vamos del Presenter a el Router de otro Modulo VIPER.
+ 
+    * goTo___________( _ : )
+ */
 
 // MARK: PRESENTER -> ROUTER
 protocol ___VARIABLE_ModuleName____PresenterToRouterProtocol: AnyObject {
