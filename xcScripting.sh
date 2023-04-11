107db8cd650493a0d837c7133af9480d01aa072c
Abraham Vazquez <mabraham.vazquez@gmail.com>
2023-04-10 23:28:11 -0600

    [UPDATE] Validate a directory
    * show the directory

--- xcScripting.sh
+++ xcScripting.sh
@@ -8,6 +8,18 @@ if [[ $1 == "" ]]
 then
   echo "El Directorio esta vacio"
 else
-  echo "El Directorio es: $directorio "
+  echo "El Directorio es: $directorio"
+  echo "$directorio"  > ~/Directorios.txt
 fi
 
+
+
+# validar que si el directorio existe, pero no hay archivos, no mandar error
+
+
+mostrar_archivo() {
+  echo "Contenido del archivo $directorio:"
+  cat $directorio
+}
+
+mostrar_archivo $directorio
